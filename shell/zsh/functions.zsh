0c546aaa2604f5d5f1bed93c58ba34fec53a929e
david 🐈 <david@fxaa.dev>
2021-09-16 11:23:44 -0600

    set up submodules

--- shell/zsh/functions.zsh
+++ shell/zsh/functions.zsh
@@ -1,38 +1,6 @@
-awsauth-help () {
-    echo
-    echo "Usage:"
-    echo "awsauth [profile]"
-    echo
-}
-
-awsauth-nonexistent-profile () {
-    echo "Profile \"$1\" does not exist"
-    awsauth-help
-}
-
-AWSAUTH_ARGS=(
-    account
-    profile
-    role
-)
-
-awsauth () {
-    if [ ${#*[@]} -ne 1 ]; then
-        awsauth-help
-        return 1
-    fi
-
-    awsauth_flags=()
-    for awsauth_arg in "${AWSAUTH_ARGS[@]}"
-    do
-        if [[ -v awsauthprofiles[$1-$awsauth_arg] ]]
-        then
-            awsauth_flags+=--$awsauth_arg="$awsauthprofiles[$1-$awsauth_arg]"
-        else
-            awsauth-nonexistent-profile $1
-            return 1
-        fi
-    done
-
-    awsauth $awsauth_flags[@]
+add-zsh-plugin-submodule () {
+    current_dir="$(pwd)"
+    cd ~/.dotfiles
+    git submodule add --name $2 https://github.com/$1/$2 shell/zsh/remote/$2
+    cd "$current_dir"
 }
---
+++ shell/zsh/znap-plugins.zsh
@@ -0,0 +1,19 @@
+znap source chrissicool/zsh-256color
+znap source mafredri/zsh-async
+znap source eendroroy/zed-zsh
+znap source changyuheng/fz
+znap source joshskidmore/zsh-fzf-history-search
+znap source reegnz/jq-zsh-plugin
+znap source zpm-zsh/ssh
+znap source Aloxaf/fzf-tab
+
+ZSH_AUTOSUGGEST_STRATEGY=( completion history )
+ZSH_AUTOSUGGEST_HISTORY_IGNORE="(cd *|git *)"
+ZSH_AUTOSUGGEST_COMPLETION_IGNORE="git *"
+znap source zsh-users/zsh-autosuggestions
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets )
+znap source zsh-users/zsh-syntax-highlighting
+
+znap source marlonrichert/zcolors
+znap eval   marlonrichert/zcolors "zcolors"
